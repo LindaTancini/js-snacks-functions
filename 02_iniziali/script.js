e17cddcfb096af68614da95af64ab7f58d29520d
LindaTancini <lindatancini95@gmail.com>
2025-03-03 15:42:32 +0100

    feat: add arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -11,6 +11,17 @@ function initial(array) {
   return newInitial;
 }
 
+// CON ARROW FUNCTION
+
+/* const initial = (array) => {
+  let newInitial = [];
+  for (let i = 0; i < array.length; i++) {
+    newInitial.push(array[i].charAt(0));
+  }
+  return newInitial;
+  }
+*/
+
 // Invoca la funzione qui e stampa il risultato in console
 const newInitial = initial(names);
 console.log(newInitial);
