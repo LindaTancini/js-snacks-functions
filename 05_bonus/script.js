3b480d4a3d07d045e2ff5d91b7e1982dacd81f0a
LindaTancini <lindatancini95@gmail.com>
2025-03-03 16:25:33 +0100

    feat: add new date

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,24 +5,24 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-function sayHi() {
-    const hours =
+function sayHi(name) {
+  const hours = new Date("03-03-2025IT18:00:00");
 
-    let greeting;
-    if (hours < 13) {
-        greeting = "Buongiorno";
-    } else if (hours < 17) {
-        greeting = "Buon Pomeriggio"
-    } else {
-        greeting = "Buonasera"
-    }
+  let greeting;
+  if (hours < 13) {
+    greeting = "Buongiorno";
+  } else if (hours < 17) {
+    greeting = "Buon Pomeriggio";
+  } else {
+    greeting = "Buonasera";
+  }
+  return `${greeting} ${name}.`;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(sayHi(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
