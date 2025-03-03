4bb6e784719749a1480e99a8ad849bc49c445c4f
LindaTancini <lindatancini95@gmail.com>
2025-03-03 14:38:41 +0100

    feat: add second snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,17 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function initial(array) {
+  let newInitial = [];
+  for (let i = 0; i < array.length; i++) {
+    newInitial.push(array[i].charAt(0));
+  }
+  return newInitial;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const newInitial = initial(names);
+console.log(newInitial);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
