00d2104210718d378b83c3f93fb695d242d017f0
LindaTancini <lindatancini95@gmail.com>
2025-03-03 15:35:02 +0100

    feat: add fourth snack with indexOf and includes

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+function countVocali(letter) {
+  const vocali = "aeiou";
+  let count = 0;
+  for (let i = 0; i < letter.length; i++) {
+    if (vocali.indexOf(word[i]) !== -1) count++;
+    //metodo includes
+    // if (vocali.includes(word[i])) count++;
+  }
+  return count;
+}
 
+//metodo includes
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const count = countVocali(word);
+console.log(count);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
