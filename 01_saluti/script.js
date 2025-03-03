7b323029a4c9ae922dc446f991ed52f740a6abd5
LindaTancini <lindatancini95@gmail.com>
2025-03-03 13:30:25 +0100

    feat: add first snack

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
+function sayHi(name) {
+  return `Ciao ${name}`;
+}
 
+// con arrow functions
+//const sayHi = (name) => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(sayHi(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
